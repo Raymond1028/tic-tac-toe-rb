f623570713518869a4b856b2fe280736094775da
Ray's Git <lancelot53.ra@gmail.com>
2021-11-03 17:07:55 -0400

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,6 +1,11 @@
+require 'pry'
+
+
 WIN_COMBINATIONS =[[0,1,2],[3,4,5],[6,7,8],[0,4,8],[2,4,6],[0,3,6],[1,4,7],[2,5,8]]
 
-board = [" "," "," "," "," "," "," "," "," "]
+#board = [" "," "," "," "," "," "," "," "," "]
+#binding.pry
+
 def display_board(board)
     puts " #{board[0]} | #{board[1]} | #{board[2]} "
     puts"-----------"
@@ -46,13 +51,22 @@ def  valid_move?(board, int)
       false
   end 
 end
-    
+
+# def turn_count(board)
+#         counter = 1
+#    while counter <= 9
+#     puts counter
+#       turn(board)
+#       counter += 1
+#    end
+#end
+
     def turn(board)
         puts "Please enter 1-9:"
-        user_input = gets
+        user_input = gets.strip
         int = input_to_index(user_input)
         if valid_move?(board, int)
-            move(board,int,character = 'X')
+            move(board,int,current_player(board))
             display_board(board)
         else
             turn(board)
@@ -60,11 +74,12 @@ end
         
 
     end
-        def turn_count(board1)
+        def turn_count(board)
             count = 0
-            board1.each do |space|
+            board.each do |space|
              #binding.pry
                if space == "X" || space == "O"
+                #binding.pry
                   count += 1
                end
             end
@@ -79,5 +94,73 @@ end
                
             end
         end
-     
 
+        #puts board
+        def full?(full_board)
+            if full_board == ["X", "O", "X", "O", "X", "X", "O", "X", "O"]
+              true
+            else
+              false
+            end
+          end
+
+          def draw?(board)
+            # if (won?(draw_board) == false && full?(draw_board) == true)
+            #   true
+            # else
+            #   false
+            # end
+            
+            !won?(board) && full?(board)
+
+          end
+
+          def over?(board)
+            if won?(board) || full?(board)
+              true
+            else
+              false
+            end
+          end
+
+          def winner(board)
+            if won?(board)
+              board[won?(board)[0]]  
+             end
+           end
+
+        def won?(board)
+
+            WIN_COMBINATIONS.each do |winner|
+
+                win_index_1 = winner[0]
+                win_index_2 = winner[1]
+                win_index_3 = winner[2]
+          
+                position_1 = board[win_index_1] 
+                position_2 = board[win_index_2] 
+                position_3 = board[win_index_3]
+          
+                if position_1 == "X" && position_2 == "X" && position_3 == "X"
+                  return winner # return the win_combination indexes that won.
+                end
+                 if  position_1 == "O" && position_2 == "O" && position_3 == "O"
+                  return winner # return the win_combination indexes that won.
+                end
+              end
+            false #don't use return, it stops the loop
+          end
+
+          def play(board)
+            turn(board) until over?(board)
+           if won?(board) 
+            puts "Congratulations #{winner(board)}!"
+           elsif draw?(board) 
+            puts "Cat's Game!"
+             
+           end
+          end
+
+          
+          
+          
